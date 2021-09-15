4274e20fa1ff6a29ff32d5d5f64b3c2477275903
Artem Grachov <grachov2195@gmail.com>
2021-09-15 09:01:22 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'nuxt-property-decorator';
+
+@Component({})
+export default class PageIndex extends Vue {}
+</script>
